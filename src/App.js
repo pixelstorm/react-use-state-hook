8417797f0c90bb2083d737462459ba6e224bd969
pixelstorm <dan@pixelstorm.com.au>
2021-07-25 15:36:56 +1000

    introduce up and down theme state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 function App() {
   const [count, setCount] = useState(3);
+  const [Theme, setTheme] = useState("neutral");
+
   const decrementCount = () => {
     setCount((prevCount) => prevCount - 1);
+    setTheme("down");
   };
   const incrementCount = () => {
     setCount((prevCount) => prevCount + 1);
+    setTheme("up");
   };
   return (
     <>
       <button onClick={decrementCount}>-</button>
-      <span>{count}</span>
+      <span>
+        {count}
+        {Theme}
+      </span>
       <button onClick={incrementCount}>+</button>
     </>
   );
