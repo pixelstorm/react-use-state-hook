ac9548c773cde9bb1b771a5938d3f9eca443f5c5
pixelstorm <dan@pixelstorm.com.au>
2021-07-25 13:50:30 +1000

    set count to 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import React, { useState } from "react";
 function App() {
+  const [count, setCount] = useState(3);
   return (
     <>
       <button>-</button>
-      <span>0</span>
+      <span>{count}</span>
       <button>+</button>
     </>
   );
